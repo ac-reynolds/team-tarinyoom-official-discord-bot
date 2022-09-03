cb50d71ae9cb82f1874045251c444138a2fdb068
Reef <reefwg@gmail.com>
2022-09-03 00:56:08 -0400

    add upsertVectors

--- scripts/podManager.js
+++ scripts/podManager.js
@@ -1,7 +1,11 @@
 var request = require('request');
 const secrets = require('./secretManager')
 
-function updateVector(id, jsonArray, guildId){
+//jsonObject properties
+//id - discord messageId
+//values - cohere embed array data
+//namespace - discord guildId
+function updateVector(jsonObject){
     return request({
         url: "https://hackathon-s1-95bcf95.svc.us-west1-gcp.pinecone.io/vectors/update",
         method: "POST",
@@ -10,12 +14,28 @@ function updateVector(id, jsonArray, guildId){
             'Content-Type': 'application/json'
         },
         json: true,   
-        body:  {
-            "id": id,
-            "values": jsonArray,
-            "namespace": guildId
-        }
+        body:  jsonObject
     }, function (error, response, body){
         console.log(response);
     });
 }
+
+
+function upsertVectors(jsonObjects){
+    return request({
+        url: "https://hackathon-s1-95bcf95.svc.us-west1-gcp.pinecone.io/vectors/upsert",
+        method: "POST",
+        headers: {
+            'Api-Key': secrets.getPineconeToken(),
+            'Content-Type': 'application/json'
+        },
+        json: true,   
+        body:  jsonObjects
+    }, function (error, response, body){
+        console.log(response);
+    });
+}
+
+
+
+module.exports = { updateVector, upsertVectors };
