413e44ea2cc7e89252ae13787f4081553fe74c6b
ac-reynolds <adam.chiu.reynolds@gmail.com>
2022-09-03 12:44:39 -0700

    discord client responds to slash commands

--- scripts/discord/discordBot.js
+++ scripts/discord/discordBot.js
@@ -1,23 +1,56 @@
 const { Client, GatewayIntentBits } = require('discord.js');
-const secrets = require('../secretManager')
+const secretManager = require('../secretManager');
+const registerCommands = require('./registerCommands');
 let client;
 
-function onReady() {
-	console.log("ready!");
+async function handleSearch(interaction) {
+	const target = interaction.options.getString('target');
+	await interaction.reply(`Searching for target: "${target}"...`);
 }
 
-function onMessageCreate(msg) {
-	console.log(`${msg} created`);
+async function handleSleuth(interaction) {
+	await interaction.reply(`Sleuthing...`);
 }
+
+function buildOnReady(secrets) {
+	return () => {
+		registerCommands.register(secrets);
+		console.log("Bot ready!");
+	};
+};
+
+function buildOnInteractionCreate() {
+	return async interaction => {
+		if (!interaction.isChatInputCommand()) return;
+
+		const { commandName } = interaction;
+
+		switch (commandName) {
+			case 'tsleuth':
+				try {
+					await handleSleuth(interaction);
+				} catch (error) {
+					console.error(`Error when responding to sleuth command: ${error}`);
+				}
+			case 'tsearch':
+				try {
+					await handleSearch(interaction);
+				} catch (error) {
+					console.error(`Error when responding to search command: ${error}`);
+				}
+		}
+	};
+};
 
 function runDiscordBot(){
 	// Create a new client instance
 	client = new Client({ intents: [GatewayIntentBits.Guilds] });
+	const secrets = secretManager.getDiscordSecrets();
 
-	client.once('ready', onReady);
-	client.on('messageCreate', onMessageCreate); // why isn't this getting triggered ?
+	client.once('ready', buildOnReady(secrets));
+	client.on('interactionCreate', buildOnInteractionCreate());
 
-	client.login(secrets.getDiscordSecrets().botToken);
-}
+	client.login(secrets.botToken);
+};
 
 module.exports = { runDiscordBot };
---
+++ scripts/discord/registerCommands.js
@@ -0,0 +1,28 @@
+const { SlashCommandBuilder, Routes } = require('discord.js');
+const { REST } = require('@discordjs/rest');
+
+const commands = [
+	new SlashCommandBuilder().setName('tsleuth').setDescription('Reads in server message data. Takes a while... don\'t disturb me!'),
+	new SlashCommandBuilder().setName('tsearch').setDescription('Searches for the given string using approximate semantic search.')
+		.addStringOption(option => 
+			option
+			.setName('target')
+			.setDescription('The target to search for!')
+			.setRequired(true))
+].map(command => command.toJSON());
+
+/**
+ * Registers the appropriate command according to the passed parameters.
+ * @param {{botToken, clientId,}} config 
+ */
+function register(config) {
+	
+	const rest = new REST({ version: '10' }).setToken(config.botToken);
+	const route = Routes.applicationCommands(config.clientId);
+	
+	rest.put(route, { body: commands })
+		.then(() => console.log('Successfully registered application commands.'))
+		.catch(console.error);
+};
+
+module.exports = { register };
