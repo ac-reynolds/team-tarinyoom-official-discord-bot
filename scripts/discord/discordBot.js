fc95d05fd49e5ddceacd634b99e23fedf3a40bbd
ac-reynolds <adam.chiu.reynolds@gmail.com>
2022-09-03 16:08:51 -0700

    user-facing search command formatting

--- scripts/discord/discordBot.js
+++ scripts/discord/discordBot.js
@@ -1,4 +1,4 @@
-const { Client, GatewayIntentBits } = require('discord.js');
+const { messageLink, time, Client, GatewayIntentBits, TimestampStyles } = require('discord.js');
 const secretManager = require('../secretManager');
 const registerCommands = require('./registerCommands');
 const sleuther = require('./sleuther');
@@ -7,19 +7,29 @@ let client;
 
 async function handleSearch(interaction) {
 
+	// Retrieve message IDs with search
 	const target = interaction.options.getString('target');
 	const results = await searcher.search(target, interaction.guildId, 1);
 
+	// Get the actual messages
 	const channels = client.guilds.cache.get(interaction.guildId).channels.cache;
+	const messages = await Promise.all(results.map(
+		async result => await channels.get(result.channelId).messages.fetch(result.messageId)));
 
-	const messages = results.map(
-		async result => await channels.get(result.channelId).messages.fetch(result.messageId));
-
-	const fetched = await Promise.all(messages);
-	for (m of fetched) {
-		console.log(await m);
+	// Format messages for output to screen
+	function formatMessage(m) {
+		let output = "";
+		output += `> ${m.content}\n`
+		output += `from \`${m.author.username}#${m.author.discriminator}\` on ${time(Math.round(m.createdTimestamp / 1000), "F")}\n`;
+		output += `<${messageLink(m.channelId, m.id, m.guildId)}>`
+		return output;
 	}
-	await interaction.reply(`Found answers for ${target}: "${fetched}"...`);
+
+	const outputData = messages
+		.map(message => formatMessage(message))
+		.join("\n\n");
+
+	await interaction.reply({ content: `Search results for "${target}":\n\n${outputData}`, ephemeral: true });
 }
 
 async function handleSleuth(interaction) {
